6f4fa23a2e130729ae6a778062e5d81b52f96799
Pinaka <157313894+pin4ka@users.noreply.github.com>
2024-01-23 00:50:19 +0530

    Update arrat&string.c
    complete game

--- arrat&string.c
+++ arrat&string.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
 int main()
 {
-    int a = 5;
-    stand(time(0)) ;
-    n = rand() ℅100 +1 ; // generate a random number between 1 to 100
-    printf(“the number is%d”,n) ;
+    srand(time(0));
+    int number = rand() % 100 + 1,user_input, count = 0;
     while (1)
+    {
+        count++;
+        printf("\nGuess the random number: ");
+        scanf("%d", &user_input);
+
+        if (user_input > number)
         {
-            if (a==n)
-            {
-                break;
-            }
+            printf("You choose a greater number\n");
         }
+        else if (user_input < number)
+        {
+            printf("You choose smaller number\n");
+        }
+        else if (user_input == number)
+        {
+            printf("you choose write number\n");
+            break;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("\nYou choose write number in %d attempts", count);
+
     return 0;
 }
