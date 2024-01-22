a012147d015a4c08beac1371f025104c7ded3c88
Akash Barman <157362347+AkashBarman1@users.noreply.github.com>
2024-01-22 16:27:26 +0530

    first file upload

---
+++ arrat&string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    // int z[7];
+    char akash[] = "hallo world";
+
+    // char b[7][]
+    // for (i = 0; i < 7; i++)
+
+    printf("%c", akash[0]);
+
+    return 0;
+}
